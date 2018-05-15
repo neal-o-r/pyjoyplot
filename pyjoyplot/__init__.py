076c896f76aa23b5d2ad74e944d0f0e2bfe64425
neal-o-r <oriainn@tcd.ie>
2018-05-15 16:25:05 +0100

    bug fixes in colors

--- pyjoyplot/__init__.py
+++ pyjoyplot/__init__.py
@@ -6,15 +6,17 @@ import numpy as np
 class _pyjoyplotter():
 
     def __init__(self, data=None, x=None, y=None, hue=None, kind=None,
-        offset=0.75, cmap='Dark2', smooth=1, order=None, bins=None, weights=None, figsize=None):
+                       offset=0.75, cmap='Dark2', smooth=1, order=None,
+                       bins=None, weights=None, figsize=None):
         ' initialise class, check args'
+
         assert (kind == 'line') or (kind == 'hist')
-        assert type(data) == pd.core.frame.DataFrame 
+        assert type(data) == pd.core.frame.DataFrame
         assert (x in data) and (hue in data), \
                 'x and hue must be columns in dataframe'
         if kind == 'line': assert (y in data), \
                 'y must be column in dataframe for line plot'
-        
+
         self.data = data
         self.x = x
         self.y = y
@@ -30,29 +32,29 @@ class _pyjoyplotter():
         self.weights = weights
         self.figsize = figsize
 
-    
+
     def _get_colours(self, cmap):
-    
-        if type(cmap) == str:
+
+        if isinstance(cmap, str):
             assert cmap in [
             m for m in plt.cm.datad if not m.endswith("_r")]
             cm = plt.get_cmap(cmap)
-            
+
             return [cm(i) for i in range(cm.N)]
 
-        if type(cmap) == list:
+        if isinstance(cmap, list):
             assert len(cmap) == self.n
-            return cmap
+            return list(reversed(cmap))
 
     def _line_plot(self):
-    
-        fig = plt.figure(figsize=figsize)
+
+        fig = plt.figure(figsize=self.figsize)
         ax = plt.axes(frameon=False)
 
         self.data = self.data.sort_values(by=self.x)
 
         for i, c in enumerate(self.categories):
-        
+
             i = self.n - (i + 1)
             df = self.data[
                 self.data[self.hue] == c
@@ -65,25 +67,25 @@ class _pyjoyplotter():
 
             y_d += i * self.offset
             y_min = np.tile(np.nanmin(y_d), (len(y_d)))
-        
+
             col = self.colours[i % self.n_c]
 
-            plt.plot(x_d, y_d, 
-                    color=col, 
+            plt.plot(x_d, y_d,
+                    color=col,
                     label=c,
                     alpha=0.8)
-                    
+
             plt.fill_between(x_d, y_d, y_min,
                     alpha=0.6,
                     color=col)
 
         x_min = self.data[self.x].min()
         x_max = self.data[self.x].max()
-        plt.xlim(x_min, x_max)  
+        plt.xlim(x_min, x_max)
         plt.xlabel(self.x)
         ax.set_yticks([self.offset*i for i in range(self.n)[::-1]])
         ax.set_yticklabels(self.categories)
-    
+
         return ax
 
 
@@ -91,17 +93,17 @@ class _pyjoyplotter():
 
         fig = plt.figure(figsize=self.figsize)
         ax = plt.axes(frameon=False)
-        if type(self.bins) != list:
+
+        if not isinstance(self.bins, list):
             self.bins = self.n * [self.bins]
         else:
             self.bins = list(reversed(self.bins))
-        if type(self.weights) != list:
+
+        if self.weights is None:
             self.weights = self.n * [self.weights]
-        else:
-            self.weights = list(reversed(self.weights))
-        
+
         for i, c in enumerate(self.categories):
-        
+
             i = self.n - (i + 1)
             df = self.data[
                 self.data[self.hue] == c]
@@ -111,19 +113,19 @@ class _pyjoyplotter():
 
             col = self.colours[i % self.n_c]
             if self.weights[i]:
-                hist = np.histogram(x_d, bins=self.bins[i], weights=x_d)
+                hist = np.histogram(x_d, bins=self.bins[i], weights=self.weights[i])
             else:
                 hist = np.histogram(x_d, bins=self.bins[i])
-            
+
             new_hist = (hist[0] / np.nanmax(hist[0]),
                     hist[1])
 
-            width = (new_hist[1] - np.roll(new_hist[1], 1))[1:]  
+            width = (new_hist[1] - np.roll(new_hist[1], 1))[1:]
             bot = i * self.offset
             left = new_hist[1][:-1]
-            height = new_hist[0]    
-            
-            plt.bar(left, height, 
+            height = new_hist[0]
+
+            plt.bar(left, height, color=col,
                     width=width, bottom=bot, alpha=0.8, align='edge')
 
         plt.xlabel(self.x)
@@ -131,10 +133,10 @@ class _pyjoyplotter():
         ax.set_yticklabels(self.categories)
 
         return ax
-    
+
 
     def _plot(self):
-        
+
         if self.kind == 'line':
             return self._line_plot()
 
@@ -142,29 +144,31 @@ class _pyjoyplotter():
             return self._hist_plot()
 
 
-def plot(data=None, x=None, y=None, hue=None, kind='line', 
-        offset=0.75, cmap='Dark2', smooth=1, order=None, bins=10, weights=None, figsize=None):
+def plot(data=None, x=None, y=None, hue=None, kind='line',
+         offset=0.75, cmap='Dark2', smooth=1, order=None,
+         bins=10, weights=None, figsize=None):
     '''
     Create 'Joy Plot':
         data (pd.DataFrame): DataFrame holding all data
         x (str)  : DataFrame column to use as x value
-        y (str)  : DataFrame column to use as y values 
+        y (str)  : DataFrame column to use as y values
         hue (str): DataFrame column to use to group data
         kind (str): specify plot type; line or hist
         offset (int/float): vertical seperation between plots
         cmap (str/list): name of matplotlib cmap, or list
-                 of colors to be used for plots
+                         of colors to be used for plots
         smooth (int): smoothing window, if smoothing to be applied
         order (list): order of categories - top to bottom
-        bins (int/list): bins if using hist. int for all hists to have same bins
-                 else list of bin no. for each hist
-        weights (boolean/list): should the histogram be weighted?
-
-    ''' 
+        bins (int/list): bins if using hist. int for all hists to have same
+                         bins else list of bin no. for each hist.
+                         If list, entries are specified from top to bottom
+        weights (list): weights for (each hue) can be None
+                        or a list of weights specified from top to bottom
+    '''
 
 
     plotter = _pyjoyplotter(data=data, x=x, y=y, hue=hue,
             offset=offset, cmap=cmap, smooth=smooth, kind=kind,
             order=order, bins=bins, weights=weights, figsize=figsize)
-    return plotter._plot()  
-    
+    return plotter._plot()
+
